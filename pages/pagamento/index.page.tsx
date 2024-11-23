a870134596ddfc9cf03b1b1315e8510572f0e4de
Matheus Ribeiro <matheusriberomateus@gmail.com>
2024-11-23 09:52:42 -0300

    finalizando a estilização da pagina de pagamento

--- pages/pagamento/index.page.tsx
+++ pages/pagamento/index.page.tsx
@@ -3,19 +3,22 @@ import { ItensDetails } from "./components/itens";
 
 export default function Pagamento() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-cednetBlue p-0 md:p-4">
-      <div className="flex w-full max-w-5xl flex-col overflow-hidden rounded-[.8rem] bg-cednetWhite shadow-lg md:flex-row">
+    <div className="relative h-screen w-screen bg-cednetBlue p-0 md:p-4">
+      {/* Faixa branca na parte inferior */}
+      <div className="bg-white md:absolute md:bottom-0 md:left-0 md:h-[23.1%] md:w-full"></div>
+
+      <div className="flex max-w-5xl flex-col bg-cednetWhite shadow-lg md:fixed md:left-[50%] md:top-[50%] md:translate-x-[-50%] md:translate-y-[-50%] md:flex-row md:rounded-[.8rem]">
         {/* Coluna da esquerda */}
         <div className="w-full p-6 md:w-3/4">
           <img
             src="/public/images/logo.png"
             alt="Cednet Logo"
-            className="mb-8 h-10"
+            className="mb-8 h-10 text-cednetBlue"
           />
           <h2 className="mb-4 text-lg font-semibold text-black">
             Todos os Itens
           </h2>
-          <div className="h-[24rem] space-y-4 overflow-y-auto">
+          <div className="scrollbar-custom h-[24rem] space-y-4 overflow-y-auto">
             {/* Lista de itens */}
             <ItensDetails />
             <ItensDetails />
@@ -32,7 +35,7 @@ export default function Pagamento() {
         </div>
 
         {/* Coluna da direita */}
-        <form className="flex w-full flex-col justify-between bg-gray-200 md:w-[30rem]">
+        <form className="flex w-full flex-col justify-between bg-gray-200 md:w-[30rem] md:rounded-r-[.8rem]">
           <div className="space-y-4 p-6">
             {/* Cartão */}
             <div className="flex items-center justify-center">
@@ -85,7 +88,7 @@ export default function Pagamento() {
             </div>
           </div>
           {/* Botão */}
-          <button className="mt-6 w-full rounded-br-[.8rem] bg-cednetButton py-3 text-lg font-semibold text-white hover:bg-cednetButtonHover">
+          <button className="mt-6 w-full bg-cednetButton py-3 text-lg font-semibold text-white hover:bg-cednetButtonHover md:rounded-br-[.8rem]">
             Pagar agora
           </button>
         </form>
